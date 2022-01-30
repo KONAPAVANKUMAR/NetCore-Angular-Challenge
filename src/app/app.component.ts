0ddbc835ff1cf5f59ab216c6cb7498a1a794440e
Kona Pavan Kumar <43029063+KONAPAVANKUMAR@users.noreply.github.com>
2022-01-30 16:00:47 +0530

    dropdown with searchbar implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['app.component.css'],
+
 })
 export class AppComponent {
-  title = 'CarProject';
+  title = 'CarProject'
+  name = ''
+  warning = ''
+  carsEndpoint = 'https://s3-ap-southeast-1.amazonaws.com/he-public-data/Cars9096be5.json'
+  cars = []
+  carsLoaded = false
+
+  constructor(){
+    this.getCars()
+  }
+
+  getCars(){
+    fetch(this.carsEndpoint)
+    .then(response => response.json())
+    .then(data => {
+      this.cars = data
+    }).then(() => {
+      this.carsLoaded = true
+    })
+  }
+
+  onTypingName(name: string){
+    // check length
+    if(this.name.length > 75){
+      this.warning = 'Cannot Exceed 75 characters'
+    }
+    // if name has a-z or A-Z only
+    else if(!this.name.match(/^[a-zA-Z]+$/) && this.name.length > 0){
+      this.warning = 'Cannot contain numbers or special characters'
+    }
+
+    else{
+      this.warning = ''
+    }
+
+  }
+
 }
