a4badcc70b4b5d1aefcd886ea70ffa3bfc054e8a
Kona Pavan Kumar <43029063+KONAPAVANKUMAR@users.noreply.github.com>
2022-01-30 18:51:43 +0530

    color pickup feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'CarProject'
   name = ''
-  warning = ''
+  warning = 'Please enter a name'
   carsEndpoint = 'https://s3-ap-southeast-1.amazonaws.com/he-public-data/Cars9096be5.json'
+
   cars = []
-  carsLoaded = false
   selectedCar = ''
+  selectedColor : any
 
   constructor(){
     this.getCars()
@@ -28,14 +28,17 @@ export class AppComponent {
     .then(response => response.json())
     .then(data => {
       this.cars = data
-    }).then(() => {
-      this.carsLoaded = true
     })
   }
 
   onTypingName(name: string){
     // check length
-    if(this.name.length > 75){
+
+    if (!name.length){
+      this.warning = 'Please enter a name'
+    }
+
+    else if(this.name.length > 75){
       this.warning = 'Cannot Exceed 75 characters'
     }
     // if name has a-z or A-Z only
